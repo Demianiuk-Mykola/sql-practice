7d15e30688fd1eddbf447766b00223e86bf9a694
Niko <mykolademianiuk@live.com>
2026-09-14 16:10:25 -0400

    9/14/26

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -96,14 +96,8 @@ UPDATE dbo.persons
  WHERE score = 0
 ----------------------------------------------------
 
-
-
-SELECT * 
-  FROM dbo.persons
-
+--Variables, Objects, Values, Refernce
 CREATE TABLE #aaa (rid INT, rname VARCHAR(20));
-
-
 
 -- in @a_0 we see real value (directly in memory), in @a_null we see refernce to memory where variable value will be assigned.
 DECLARE @a_0 VARCHAR(20) = 0, @a_null VARCHAR (20); 
@@ -121,13 +115,13 @@ PRINT ISNULL ( @rId, 'my variable is NULL now');
 
 DECLARE @rId int, @rName VARCHAR (20); 
 DECLARE @aaa TABLE (rId INT, rName VARCHAR(20))
-INSERt INTO @aaa SELECT 1,'first record';
-INSERt INTO @aaa SELECT 2,'second record';
+INSERT INTO @aaa SELECT 1,'first record';
+INSERT INTO @aaa SELECT 2,'second record';
 SELECT 'All Records', * FROM @aaa;
 SELECT Top 1 @rId = rId, @rName = rName FROM @aaa order by rID Asc ;  SELECT '1st record assigned/print out', @rId, @rName
 SELECT TOP 1 @rId = rId, @rName = rName FROM @aaa ORDER BY rId Desc;  SELECT '2nd record assigned/print out', @rId, @rName
 SELECT @rId = rId, @rName = rName FROM @aaa;                          SELECT 'Iterate all/print out last', @rId, @rName
-
+-------------------------------------------------------------------------------------------
 -- Schema creation and deletion (schema can be deleted when no tables are in)
 CREATE SCHEMA Nik;
 CREATE TABLE Nik.aaa (rid INT, rname VARCHAR(20));
@@ -153,5 +147,19 @@ SELECT [type],[type_desc], COUNT(1)
   FROM sys.all_objects
  GROUP BY [type], [type_desc]
 
-
+------------------------------------------------------------------------
 SELECT (sqrt(101)-sqrt(99)), sqrt(101), sqrt(99)
+
+SELECT *
+  FROM dbo.customers c
+ WHERE c.score BETWEEN 100 AND 500
+
+SELECT * FROM customers
+
+SELECT *
+  FROM customers
+ WHERE first_name LIKE '%N'
+
+ SELECT *
+  FROM customers
+ WHERE first_name LIKE '_a__%'
